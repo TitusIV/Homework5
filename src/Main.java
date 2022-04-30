8739259a6a62df70e248b0923e5ff02c89c5a7b1
TitusIV <104397318+TitusIV@users.noreply.github.com>
2022-04-30 12:49:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        int[] trio = new int[3];
+        trio[0] = 1;
+        trio[1] = 2;
+        trio[2] = 3;
+        System.out.println(trio[0]);
+        System.out.println(trio[1]);
+        System.out.println(trio[2]);
+
+        float[] fraction = {1.57f, 7.654f, 9.986f};
+        System.out.println(fraction[0]);
+        System.out.println(fraction[1]);
+        System.out.println(fraction[2]);
+
+        char[] alhabet = {'a', 'b', 'c'};
+        System.out.println(alhabet[0]);
+        System.out.println(alhabet[1]);
+        System.out.println(alhabet[2]);
+
+        // Задача 2
+        System.out.println("Задача 2");
+        System.out.println(trio[0] + ", " + trio[1] + ", " + trio[2]);
+        System.out.println(fraction[0] + ", " + fraction[1] + ", " + fraction[2]);
+        System.out.println(alhabet[0] + ", " + alhabet[1] + ", " + alhabet[2]);
+
+        // Задача 3
+        System.out.println("Задача 3");
+        System.out.println(trio[2] + ", " + trio[1] + ", " + trio[0]);
+        System.out.println(fraction[2] + ", " + fraction[1] + ", " + fraction[0]);
+        System.out.println(alhabet[2] + ", " + alhabet[1] + ", " + alhabet[0]);
+
+        // Задача 4
+        System.out.println("Задача 4");
+        
+
+    }
+}
+
